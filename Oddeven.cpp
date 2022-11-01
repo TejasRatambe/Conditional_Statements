3a937185aaf26163ed6bbbf2aa20b15ddfb68c06
Tejas Ratambe <99137410+TejasRatambe@users.noreply.github.com>
2022-11-01 09:24:49 +0530

    Conditional Statements in C++

---
+++ Oddeven.cpp
@@ -0,0 +1,19 @@
+//WAP to Print Given No is Even or Odd
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin>>num;
+	
+	if(num%2==0){
+		cout<<"Even"<<endl;
+	}
+	else{
+		cout<<"Odd"<<endl;
+	}
+	
+	return 0;
+}
